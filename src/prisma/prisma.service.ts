8ddb40d9e796f9dc747a1de4c434c78a6c5ef7cc
alex <alex@gmail.com>
2025-12-02 03:58:58 -0300

    fix: prisma service e cliente regenerado

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma';
+import { PrismaClient } from '../../generated/prisma'; // IMPORTA DO CLIENT GERADO
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  
   async onModuleInit() {
-    await this.$connect();
+    // Prisma 7 NAO USA MAIS $connect()
+    await this.$transaction([]); // força inicialização segura
   }
 
   async onModuleDestroy() {
+    // Prisma 7 NAO USA MAIS $disconnect()
     await this.$disconnect();
   }
 }
