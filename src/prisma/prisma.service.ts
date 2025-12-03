eb8ced4e226c5b8a62cb15b36ddd930342a4bfd2
alex <alex@gmail.com>
2025-12-03 10:45:24 -0300

    deploy inicial

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,18 +1,23 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    // PRISMA 7 NÃO USA MAIS $connect()
-    await this.$transaction([]); 
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  constructor() {
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    super({
+      adapter,
+    });
   }
 
   async onModuleDestroy() {
-    // PRISMA 7 NÃO USA MAIS $disconnect()
     await this.$disconnect();
   }
 }
