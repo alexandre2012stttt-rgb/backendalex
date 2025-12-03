bfca62ef7777c89a77691d41db740e622985de4b
alex <alex@gmail.com>
2025-12-03 15:36:39 -0300

    fix prisma 7.1.0

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,18 +1,16 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
 
-    super({ adapter }); // PRISMA 7 EXIGE ISSO
+    // Prisma 7 exige adapter dentro do super()
+    super({ adapter });
   }
 
   async onModuleInit() {
